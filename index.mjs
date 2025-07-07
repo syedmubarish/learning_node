45d57403139baf740a88711bf03bb555daab03e7
= <=>
2025-07-07 12:45:09 +0530

    named export first pattern

--- index.mjs
+++ index.mjs
@@ -1,4 +1,4 @@
-import math from './math.mjs'
+import * as math from './math.mjs'
 
 const resultAdd = math.add(2,3)
 const resultSub = math.subtract(7,3)
--- math.mjs
+++ math.mjs
@@ -1,9 +1,8 @@
-const add = (a,b)=>{
+export const add = (a,b)=>{
     return a + b
 }
 
-const subtract = (a,b)=>{
+export const subtract = (a,b)=>{
     return a-b
 }
 
-export default {add,subtract}
