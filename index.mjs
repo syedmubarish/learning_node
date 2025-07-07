c509414e8eb76e797221f5361e22e62e5cb03745
= <=>
2025-07-07 12:45:59 +0530

    named export second pattern

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
-import * as math from './math.mjs'
+import {add,subtract} from './math.mjs'
 
-const resultAdd = math.add(2,3)
-const resultSub = math.subtract(7,3)
+const resultAdd = add(2,3)
+const resultSub = subtract(7,3)
 
 console.log(resultAdd);
 console.log(resultSub);
